2975203c35098d7b3cbef90559528bbf1a9cefd6
kenchanq8 <knb.k@icloud.com>
2024-04-28 22:29:20 +0300

    os project test

--- test.py
+++ test.py
@@ -1,19 +1,23 @@
-import time
+# import time
+#
+# def start_timer():
+#     """Starts the timer."""
+#     return time.time()
+#
+# def stop_timer(start_time):
+#     """Stops the timer and returns the elapsed time."""
+#     return time.time() - start_time
+#
+# # Example usage:
+# start_time = start_timer()
+#
+# # Do something that takes time
+# time.sleep(2)
+#
+# elapsed_time = stop_timer(start_time)
+# print("Elapsed time:", elapsed_time, "seconds")
 
-def start_timer():
-    """Starts the timer."""
-    return time.time()
-
-def stop_timer(start_time):
-    """Stops the timer and returns the elapsed time."""
-    return time.time() - start_time
-
-# Example usage:
-start_time = start_timer()
-
-# Do something that takes time
-time.sleep(2)
-
-elapsed_time = stop_timer(start_time)
-print("Elapsed time:", elapsed_time, "seconds")
-
+list = [1, 2, 3, 4, 5]
+list = list[1:]
+for i in range(len(list)):
+    print(list[i])
